c5c60411c28ac4bbbba9a9632e103c9f2f376d82
RishabJankeeram <rishabjankeeram123@gmail.com>
2024-10-09 10:20:42 +0300

    changed username- checking commit

--- Day9_exercises.py
+++ Day9_exercises.py
@@ -77,3 +77,22 @@ else:
  * Check if the person dictionary has skills key, if so check if the person has 'Python' skill and print out the result.
  * If a person skills has only JavaScript and React, print('He is a front end developer'), if the person skills has Node, Python, MongoDB, print('He is a backend developer'), if the person skills has React, Node and MongoDB, Print('He is a fullstack developer'), else print('unknown title') - for more accurate results more conditions can be nested!
  * If the person is married and if he lives in Finland, print the information in the following format:'''
+
+person={
+    'first_name': 'Asabeneh',
+    'last_name': 'Yetayeh',
+    'age': 250,
+    'country': 'Finland',
+    'is_marred': True,
+    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'
+    }
+}
+
+if skills in person:
+    #print(person['skills'][3])
+    #if "python" in person['skills'][]
+        print('present')
+
